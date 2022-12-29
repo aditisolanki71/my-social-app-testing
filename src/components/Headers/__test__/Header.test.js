6252c19847adf6d854579df199b37e06a9033fb7
Aditi <adtsolanki22@gmail.com>
2022-12-29 15:15:54 +0530

    Describe Block

--- src/components/Headers/__test__/Header.test.js
+++ src/components/Headers/__test__/Header.test.js
@@ -1,23 +1,25 @@
 import { render, screen } from "@testing-library/react"
 import Header from "../Headers"
 
-//Unit Test
-test("should render same text passed into title props",() => {
-    render(<Header title="my header"/>)
-    const headingElement = screen.getByText(/my header/i)
-    expect(headingElement).toBeInTheDocument();
-});
+describe("Header", () => {
+    //Unit Test
+    test("should render same text passed into title props",() => {
+        render(<Header title="my header"/>)
+        const headingElement = screen.getByText(/my header/i)
+        expect(headingElement).toBeInTheDocument();
+    });
 
-//Semantic Test
-it("should render same text passed into title props",async () => {
-    render(<Header title="Semantic Test"/>)
-    const headingElement = screen.getByTitle("headerTitle",{name: "Semantic Test"});
-    expect(headingElement).toBeInTheDocument();
-});
+    //Semantic Test
+    it("should render same text passed into title props",async () => {
+        render(<Header title="Semantic Test"/>)
+        const headingElement = screen.getByTitle("headerTitle",{name: "Semantic Test"});
+        expect(headingElement).toBeInTheDocument();
+    });
 
-// test id
-it("should render same text passed into title props",async () => {
-    render(<Header title="Test Id"/>)
-    const headingElement = screen.getByTestId("headerTestId", {name: "Test Id"});
-    expect(headingElement).toBeInTheDocument();
-});
+    // test id
+    it("should render same text passed into title props",async () => {
+        render(<Header title="Test Id"/>)
+        const headingElement = screen.getByTestId("headerTestId", {name: "Test Id"});
+        expect(headingElement).toBeInTheDocument();
+    });
+})
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,43 +10,52 @@ const MockTodoFooter = ({numberOfIncompleteTasks}) => {
         </BrowserRouter>
     )
 }
-test("should render correct amount of incomplete task", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={5}/>);
-    const paragraphElement = screen.getByText(/5 tasks left/i)
-    expect(paragraphElement).toBeInTheDocument();
-});
+describe("Todo Footer", () => {
+    test("should render correct amount of incomplete task", async () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={5}/>);
+        const paragraphElement = screen.getByText(/5 tasks left/i)
+        expect(paragraphElement).toBeInTheDocument();
+    });
+    
+    test("should render 'task' when the number of incomplete tasks is one", async () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+        const paragraphElement = screen.getByText(/1 task left/i)
+        expect(paragraphElement).toBeInTheDocument();
+    });    
+})
 
-test("should render 'task' when the number of incomplete tasks is one", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByText(/1 task left/i)
-    expect(paragraphElement).toBeInTheDocument();
-});
+describe("Todo Footer cosmetic change", () => {
+    describe("Functionality1", () => {
+        test("should check value is visible or truthy", async () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+            const paragraphElement = screen.getByText(/1 task left/i)
+            expect(paragraphElement).toBeTruthy();
+            expect(paragraphElement).toBeVisible()
+        });
+        
+        test("should check HTML tag is exist or not", async () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+            const paragraphElement = screen.getByText(/1 task left/i)
+            expect(paragraphElement).toContainHTML("p")
+            expect(paragraphElement).toHaveTextContent("1 task left")
+        });    
+    })
+   
+    describe("functionality2", () => {
+        //assertion with test id
+        test("should check HTML Role and Content", async () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+            const paragraphElement = screen.getByTestId("para");
+            expect(paragraphElement).toHaveTextContent("1 task left")
+        });
 
-test("should render 'task' when the number of incomplete tasks is one", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByText(/1 task left/i)
-    expect(paragraphElement).toBeTruthy();
-    expect(paragraphElement).toBeVisible()
-});
+        //assertion with test id not
+        test("should check HTML Role and Content", async () => {
+            render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
+            const paragraphElement = screen.getByTestId("para");
+            expect(paragraphElement).not.toBeFalsy();
+            expect(paragraphElement.textContent).toBe("1 task left");
+        });
+    })
+})
 
-test("should check HTML tag is exist or not", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByText(/1 task left/i)
-    expect(paragraphElement).toContainHTML("p")
-    expect(paragraphElement).toHaveTextContent("1 task left")
-});
-
-//assertion with test id
-test("should check HTML Role and Content", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByTestId("para");
-    expect(paragraphElement).toHaveTextContent("1 task left")
-});
-
-//assertion with test id not
-test("should check HTML Role and Content", async () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1}/>);
-    const paragraphElement = screen.getByTestId("para");
-    expect(paragraphElement).not.toBeFalsy();
-    expect(paragraphElement.textContent).toBe("1 task left");
-});
